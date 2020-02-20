177d095c9781548fe2269f54e5c55d1ca70bac36
Anton <arnellomutty2@gmail.com>
2020-02-20 14:45:09 +0200

    Register is completed (FE)

--- frontend/src/modules/user/RegisterComponent.tsx
+++ frontend/src/modules/user/RegisterComponent.tsx
@@ -1,13 +1,74 @@
-import React, { ReactElement } from 'react'
+import React, { useState, ReactElement } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
+import { Register, RegisterVariables } from "./types/Register";
+import { RouteComponentProps } from "react-router-dom";
 
-// interface Props {
-  
-// }
+const REGISTER = gql`
+  mutation Register($email: String!, $password: String!) {
+    register(email: $email, password: $password)
+  }
+`;
 
-export default function RegisterComponent(): ReactElement {
+interface Props {
+ route: RouteComponentProps
+}
+
+export default function RegisterComponent(props: RouteComponentProps<{}>): ReactElement {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [register, { data }] = useMutation<Register, RegisterVariables>(REGISTER);
+
+  const { email, password } = state;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await register({ variables: state });
+    props.history.push("/login")
+    console.log(data);
+  };
+
   return (
-    <div>
-      Register Componnet
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "20px"
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter email..."
+            value={email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Enter password..."
+            value={password}
+            onChange={handleChange}
+            name="password"
+          />
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
